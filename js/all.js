1ef6ad1e732797bb277b68797972a428aa078741
Erice1211 <118951567+Erice1211@users.noreply.github.com>
2023-02-13 09:53:10 +0800

    初始資料上傳

---
+++ js/all.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+    $('.dropdown').click(function (event) {
+        /* Act on the event */
+        event.preventDefault();
+        //   $('.dropdown').toggleClass('active');
+        $('.dropdown-open').slideToggle();
+    });
+
+    // lightbox 效果 (以2秒的方式顯示出圖片)
+    lightbox.option({
+        'resizeDuration': 300, //圖片飛入出現的效果
+        //'fadeDuration':2000,   //圖片淡出
+        'wrapAround': true
+    });
+
+    const swiper = new Swiper('.swiper', { //在swiper的class下swiper的效果
+        // Optional parameters
+        //direction: 'vertical',  /*上下切換(預設為左右切換)*/
+        direction: 'horizontal',
+        speed: 1000, //輪播速度
+        loop: true, //是否重複輪播
+        autoplay: { //自動撥放圖片
+            delay: 1500,
+        },
+        //effect:'cube',   //輪播的特效(立方體)
+        effect: 'slide', //輪播的特效(立方體)
+        // If we need pagination (頁碼)
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar (加入捲軸)
+        // scrollbar: {
+        //   el: '.swiper-scrollbar',
+        // },
+    });
+
+    //lightbox的js設定
+    // lightbox.option({
+    //     'resizeDuration': 1000,
+    //     'wrapAround': true,
+    //     'imageFadeDuration': 1000,
+    // });
+
+
+
+});

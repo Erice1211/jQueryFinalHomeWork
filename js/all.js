b7fd6b55db8d0cff001b96466908fd4b1f2d04d2
Erice1211 <118951567+Erice1211@users.noreply.github.com>
2023-03-11 07:24:15 +0800

    助教建議的部份做修改

--- js/all.js
+++ js/all.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
-    $('.dropdown').click(function (event) {
-        /* Act on the event */
-        event.preventDefault();
-        //   $('.dropdown').toggleClass('active');
-        $('.dropdown-open').slideToggle();
-    });
+    // $('.dropdown').click(function (event) {
+    //     /* Act on the event */
+    //     event.preventDefault();
+    //     //   $('.dropdown').toggleClass('active');
+    //     $('.dropdown-open').slideToggle();
+    // });
 
     // lightbox 效果 (以2秒的方式顯示出圖片)
     lightbox.option({
@@ -63,9 +63,9 @@ $(document).ready(function () {
 
 
 
-    $('.menu>li>a').click(function (e) { 
+    $('.menu>li>a').click(function (e) {
         e.preventDefault();
-        $(this).parent().find('.dropdown-open').slideToggle();  /*被點選的判斷開或關*/
+        $(this).parent().find('.dropdown-open').slideToggle(); /*被點選的判斷開或關*/
         $(this).parent().siblings().find('.dropdown-open').slideUp(); /*被點選以外的其他選單要關掉*/
     });
 
